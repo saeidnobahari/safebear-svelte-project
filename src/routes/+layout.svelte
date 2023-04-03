4313efa2cbecf1a56b1783a367cec6df291f126f
saeid nobahari <saeidnobahari1@gmail.com>
2023-04-03 06:21:24 -0700

    init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+
+	<main>
+		<slot />
+	</main>
+	
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
